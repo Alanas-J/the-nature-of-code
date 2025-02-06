f2c2945091d405d86f5b3ff2e8d60d2385be6de8
Alanas J <34601418+Alanas-J@users.noreply.github.com>
2025-02-06 20:03:05 +0000

    Implemented the gaussian distribution exercise

---
+++ the-nature-of-code/chapter-0/alanas/0.4-gaussian-distribution.js
@@ -0,0 +1,35 @@
+
+
+export function setup() {
+  // Figured out that titles are addable:
+  let cpTitle = createP("0.4 Gaussian Distribution Excercise");
+  cpTitle.style("width", "640px");
+  cpTitle.style("font-size", "14pt");
+  cpTitle.style("font-weight", "bold");
+
+  createCanvas(640, 240);
+  background(255);
+  noStroke();
+  colorMode(HSL);
+}
+
+export function draw() {
+  const xDeviation = randomGaussian(width/2, width/6);
+  const yDeviation = randomGaussian(height/2, height/6);
+
+  const colourDeviation = randomGaussian(215, 20);
+  fill(colourDeviation, 255, 80, .750);
+  circle(xDeviation, yDeviation, 16);
+}
+
+
+// One thing to write down, I had forgotten how to calculate the standard deviation.
+// It's just pythagoras theorem seeing each data entry from the mean as it's own distinct axis/distance.
+// (Only difference is the squared distances are averaged, instead of taken as a sum) 
+
+// I guess it's the hypothenuse of the average deviation technically
+
+// Formula just to write out: 
+// -(For each entry) Get the squared distances from the mean.
+// - get the average of these distances
+// - Square root it.
