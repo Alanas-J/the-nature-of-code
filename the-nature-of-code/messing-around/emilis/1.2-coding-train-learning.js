629837abad35b31f9410c758075f9b067f1a8b71
Emilis-Codes <emilisxfl@gmail.com>
2025-02-07 17:36:23 +0000

    emilis - finished - 1.4

--- the-nature-of-code/messing-around/emilis/1.2-coding-train-learning.js
+++ the-nature-of-code/messing-around/emilis/1.2-coding-train-learning.js
@@ -6,5 +6,18 @@ export function setup() {
   
 export function draw() {
     background(220,0,200);
+    
+    let squareLength = 100;
+    rect(150, 150, squareLength, squareLength)
+
+    strokeWeight(10)
+    point (140,120)
+
+    // strokeWeight(10)
+
+    point (150+squareLength+10,120)
+
+    line (150, 300, 250, 300)
 }
+
   
